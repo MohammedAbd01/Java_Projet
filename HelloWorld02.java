f1956ac2e36caff88f9dc0238664fa4ebd88ab3b
Mohamed larbi Benjelloun <Mohamedbenjelloun223@gmail.com>
2024-12-09 22:41:51 +0100

    Update HelloWorld02.java
    affiche voici mon changement

--- HelloWorld02.java
+++ HelloWorld02.java
@@ -1,5 +1,6 @@
 public class HelloWorld02 {
     public static void main(String[] args) {
         System.out.println("Bonjour, GitHub !");
+        System.out.println("voici mon changement");
     }
 }
